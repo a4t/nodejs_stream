d0fe9d6467c02d741b92cb1d70e1d9620b54eddf
a4t <iwanomoto@gmail.com>
2016-08-29 19:48:11 +0900

    debug削除版

--- async.js
+++ async.js
@@ -3,10 +3,7 @@
 const fs = require('fs')
 const fileName = './file'
 
-console.time('first-buffer-timer');
 var buf = fs.readFile(fileName, (err, data) => {
   data.toString();
-  console.timeEnd('first-buffer-timer');
 });
 
-console.log('async')
--- sync.js
+++ sync.js
@@ -3,7 +3,5 @@
 const fs = require('fs')
 const fileName = './file'
 
-console.time('process-end-timer');
 var data = fs.readFileSync(fileName)
 data.toString();
-console.timeEnd('process-end-timer');
